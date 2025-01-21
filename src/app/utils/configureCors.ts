2c3833f540f407f3e89f609ac18bff974ee9e485
codemine24 <codemine24@gmail.com>
2025-01-21 13:16:02 +0600

    configureCors function modified

--- src/app/utils/configureCors.ts
+++ src/app/utils/configureCors.ts
@@ -1,7 +1,7 @@
 import { Request } from "express";
 
 function configureCors(req: Request, callback: (error: Error | null, options: any) => void): void {
-    const allowedDomains = ["https://dreamestate-cr.vercel.app"];
+    const allowedDomains = ["https://dreamestate-cr.vercel.app", "https://de.smartreach.dev"];
     const origin = req.headers.origin;
     const corsOptions = {
         origin: allowedDomains.includes(origin as string) || origin?.includes("localhost"),
