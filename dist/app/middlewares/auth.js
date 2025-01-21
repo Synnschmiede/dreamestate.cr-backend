082e5599df9245d2622533c3acd434c96455df50
codemine24 <codemine24@gmail.com>
2025-01-21 11:38:31 +0600

    update jwt error

--- dist/app/middlewares/auth.js
+++ dist/app/middlewares/auth.js
@@ -12,12 +12,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const ApiError_1 = __importDefault(require("../error/ApiError"));
-const http_status_1 = __importDefault(require("http-status"));
-const jwtHelpers_1 = require("../utils/jwtHelpers");
-const config_1 = __importDefault(require("../config"));
-const prisma_1 = __importDefault(require("../shared/prisma"));
 const client_1 = require("@prisma/client");
+const http_status_1 = __importDefault(require("http-status"));
+const config_1 = __importDefault(require("../config"));
+const ApiError_1 = __importDefault(require("../error/ApiError"));
+const prisma_1 = __importDefault(require("../shared/prisma"));
+const jwtHelpers_1 = require("../utils/jwtHelpers");
 const auth = (...roles) => {
     return (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
         try {
@@ -25,19 +25,25 @@ const auth = (...roles) => {
             if (!token) {
                 throw new ApiError_1.default(http_status_1.default.UNAUTHORIZED, "You are not authorized");
             }
-            const verifiedUser = (0, jwtHelpers_1.verifyToken)(token, config_1.default.jwt_access_secret);
+            let verifiedUser;
+            try {
+                verifiedUser = (0, jwtHelpers_1.verifyToken)(token, config_1.default.jwt_access_secret);
+            }
+            catch (error) {
+                throw new ApiError_1.default(http_status_1.default.UNAUTHORIZED, "Session expired");
+            }
             const user = yield prisma_1.default.user.findUniqueOrThrow({
                 where: {
-                    id: verifiedUser.id,
+                    id: verifiedUser === null || verifiedUser === void 0 ? void 0 : verifiedUser.id,
                     is_deleted: false,
                     status: client_1.UserStatus.ACTIVE,
                 },
             });
             const passwordChangedTime = Math.floor(new Date(user.password_changed_at).getTime() / 1000);
-            if (passwordChangedTime > verifiedUser.iat) {
+            if (verifiedUser && passwordChangedTime > verifiedUser.iat) {
                 throw new ApiError_1.default(http_status_1.default.UNAUTHORIZED, "Password changed recently");
             }
-            if ((roles === null || roles === void 0 ? void 0 : roles.length) && !roles.includes(verifiedUser === null || verifiedUser === void 0 ? void 0 : verifiedUser.role)) {
+            if (verifiedUser && (roles === null || roles === void 0 ? void 0 : roles.length) && !roles.includes(verifiedUser === null || verifiedUser === void 0 ? void 0 : verifiedUser.role)) {
                 throw new ApiError_1.default(http_status_1.default.UNAUTHORIZED, "You are not authorized");
             }
             req.user = verifiedUser;
--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,11 +1,11 @@
-import { NextFunction, Request, Response } from "express";
-import ApiError from "../error/ApiError";
-import httpStatus from "http-status";
-import { verifyToken } from "../utils/jwtHelpers";
-import config from "../config";
-import prisma from "../shared/prisma";
 import { UserStatus } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import config from "../config";
+import ApiError from "../error/ApiError";
 import { TAuthUser } from "../interfaces/common";
+import prisma from "../shared/prisma";
+import { verifyToken } from "../utils/jwtHelpers";
 
 const auth = (...roles: string[]) => {
   return async (
@@ -19,11 +19,17 @@ const auth = (...roles: string[]) => {
         throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized");
       }
 
-      const verifiedUser = verifyToken(token, config.jwt_access_secret);
+      let verifiedUser;
+
+      try {
+        verifiedUser = verifyToken(token, config.jwt_access_secret);
+      } catch (error) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, "Session expired");
+      }
 
       const user = await prisma.user.findUniqueOrThrow({
         where: {
-          id: verifiedUser.id,
+          id: verifiedUser?.id,
           is_deleted: false,
           status: UserStatus.ACTIVE,
         },
@@ -33,14 +39,14 @@ const auth = (...roles: string[]) => {
         new Date(user.password_changed_at).getTime() / 1000
       );
 
-      if (passwordChangedTime > verifiedUser.iat) {
+      if (verifiedUser && passwordChangedTime > verifiedUser.iat) {
         throw new ApiError(
           httpStatus.UNAUTHORIZED,
           "Password changed recently"
         );
       }
 
-      if (roles?.length && !roles.includes(verifiedUser?.role)) {
+      if (verifiedUser && roles?.length && !roles.includes(verifiedUser?.role)) {
         throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized");
       }
 
