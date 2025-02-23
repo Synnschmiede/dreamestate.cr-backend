5c3b1848c4a55c01e9f554d756354363c4a3c356
codemine24 <codemine24@gmail.com>
2025-02-23 11:36:09 +0600

    update

--- dist/app/module/File/File.constants.js
+++ dist/app/module/File/File.constants.js
@@ -9,6 +9,7 @@ exports.allowedFileType = [
     "image/svg+xml",
     "image/vnd.microsoft.icon",
     "image/webp",
+    "image/avif"
 ];
 exports.fileSortableFields = ["name", "created_at", "updated_at"];
 exports.fileSearchableFields = ["name", "type"];
--- src/app/module/File/File.constants.ts
+++ src/app/module/File/File.constants.ts
@@ -7,6 +7,7 @@ export const allowedFileType = [
     "image/svg+xml",
     "image/vnd.microsoft.icon",
     "image/webp",
+    "image/avif"
 ];
 
 export const fileSortableFields = ["name", "created_at", "updated_at"];
